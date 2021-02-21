053a14da80ff1cb2b1510d16826f3357dda92fbc
Asad Saeeduddin <masaeedu@gmail.com>
2021-02-21 00:12:06 -0500

    Add note

--- src/Data/Bifunctor/Bimodule.purs
+++ src/Data/Bifunctor/Bimodule.purs
@@ -3,6 +3,9 @@ module Data.Profunctor.Bimodule where
 import Control.Category.Tensor (class Associative)
 import Data.Profunctor (class Profunctor)
 
+-- TODO: This is too restrictive, we only need `l` and `r` to be monoidal actions, not full
+-- tensors. Generalize this.
+
 class (Associative l c, Associative r c, Profunctor p) <= LeftModule c l r p
   where
   lstrength :: forall a b x. c (p a b) (p (l a x) (r b x))
